8bc685cfe708b41d254379ec8f2a4f155cad4f20
flintdk <92164637+flintdk@users.noreply.github.com>
2022-02-14 22:19:11 +0000

    First pass at the data loader. Won't connect to db

--- dwmb_data_loader/data_loader.py
+++ dwmb_data_loader/data_loader.py
@@ -1,85 +1,91 @@
-from opcode import hasconst
+# 2022-02-14 TK/JS/WO'D; "Dude Where's My Bike" Project
+#          Comp30830 Software Engineering 
+"""
+PseudoCode Solution:
+
+define a function which
+    return 
+
+define a main function
+    print finished.
+
+if script is being run as a script (not imported) call main() function
+
+"""
+
+import sys
 import requests
 import json
-from pprint import pprint
-from IPython.display import JSON
-import sqlalchemy
+import traceback
+import sqlalchemy as db
 
-payload = {'key1': 'value1', 'key2': 'value2',
-           'key3': 'value3', 'key4': 'value4'}
-URI = 'https://bla'
-r = requests.get('URI', params=payload)
-print r.url
+def loadCredentials():
+    """Load the credentials required for accessing the JCDecaux API
 
+    Returns a JSON object with the required credentials.
+    Implemented in a method as Credential storage will be subject to change.
+    """
+    # Our credentials are just stored in a JSON file (for now)
+    # This file is not saved to GitHub and is placed on each EC2 instance
+    # by a team member.
+    # Load the JSON file
+    file = open('dudewmb.json')
+    credentials = json.load(file)
+    file.close  # Can close the file now we have the data loaded...
+    return credentials
 
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-    else:
-    print("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
-    scope = 'user-top-read'
-    token = util.prompt_for_user_token(username, scope)
+def saveToDatabase(jsonData, credentials):
+    connectionString = "mysql+mysqlconnector://" \
+        + credentials['amazonrds']['username'] + ":" + credentials['amazonrds']['password'] \
+        + "@" \
+        + credentials['amazonrds']['endpoint'] + ":3306" \
+        + "/dudeWMB?charset=utf8mb4"
+    print("Connection String: " + connectionString)
+    engine = db.create_engine(connectionString)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    sp.trace = False
-    ranges = ['short_term', 'medium_term', 'long_term']
-for range in ranges:
-    print("range:", range)
-    results = sp.current_user_top_tracks(time_range=range, limit=50)
-for i, item in enumerate(results['items']):
-    print(i, item['name'], '//', item['artists'][0]['name'])
-    print()
+    connection = engine.connect()
+    result = connection.execute("select * from Users")
+    for row in result:
+        print("firstname:", row['firstname'], "lastname:", row['lastname'])
+    connection.close()
 
-    
-    Station:
-    https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=
-    https://api.jcdecaux.com/vls/v1/stations/10?contract=dublin&apiKey=
-    Resonse when contract exitst: http/1.1 200 OK
-    Content-type: application/json
-    Response when not: http/1.1 400 Bad Request
-
-APIKEY = “secret”
-NAME = "Dublin"
-STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
-r = requests.get(STATIONS_URI, params={"apiKey": APIKEY,
-    "contract": NAME})
-
-    Dynamic Data:
-
-
-def main():
-# run forever...
-while True:
-    try:
-        r = requests.get(STATIONS,
-        params={"apiKey": APIKEY, "contract": NAME})
-        store(json.loads(r.text))
-        # now sleep for 5 minutes
-        time.sleep(5*60)
-    except:
-        # if there is any problem, print the traceback
-        print traceback.format_exc()
     return
 
-conda install sqlalchemy
-conda install -c anaconda mysql-connector-python
-    sqlalchemy
-# default
-engine = create_engine('mysql://scott:tiger@localhost/foo')
-# mysql-python
-engine = create_engine('mysql+mysqldb://scott:tiger@localhost/foo')
-# MySQL-connector-python
-engine = create_engine('mysql+mysqlconnector://scott:tiger@localhost/foo')
-# OurSQL
-engine = create_engine('mysql+oursql://scott:tiger@localhost/foo')
+def main():
+    """Load Data from the JCDecaux API
 
 
-    engine = create_engine(“mysql+mysql-connector://
-admin:pass@dbikes.ci1fi3edpsps.eu-west-1.rds.amazonaws.com/
-dbikes:3306”)
-connection = engine.connect()
-result = connection.execute("select username from users")
-for row in result:
-print("username:", row['username'])
-connection.close()
+    """
+    credentials = loadCredentials()
+
+    # "username": "tomas.kelly1@ucdconnect.ie",
+        # "password": "lD6>hD4_aP2+yV6?",
+        # "api-key": "39fa6066a9e6c316e1bb1f4acaa31b1af3b73c05"
+
+    try:
+        # Retrieve the Station/Activity Data:
+        # Set the request parameters in JSON format
+        parameters = {'contract': 'dublin', 'apiKey': credentials['jcdecaux']['api-key']}
+        # Hard-code the Station Data URI
+        uri = 'https://api.jcdecaux.com/vls/v1/stations'
+        response = requests.get(uri, params=parameters)
+    
+        if (response.status_code == 200):
+            # response.content  # Bytes
+            # response.text  # String
+            saveToDatabase(response.json(), credentials)
+        else:
+            # Our call to the API failed for some reason...  print some information
+            # Who knows... someone may even look at the logs!!
+            print("ERROR: Call to JCDecaux API failed with status code: ", response.status_code)
+            print("       The response reason was \'" + str(response.reason) + "\'")
+
+    except:
+        # if there is any problem, print the traceback
+        print(traceback.format_exc())
+    
+    print('\r\nFinished!')
+    sys.exit()
+
+if __name__ == '__main__':
+    main()
