6c0260471526f0b684f9736e396789347465ab03
Jorg8383 <jorg.striebel@ucdconnect.ie>
2022-04-01 23:17:57 +0100

    20220401 JS; data_resampler.py --> bugfix in getResampleTimeWindowForPreviousHour()

--- dwmb_data_loader/data_resampler.py
+++ dwmb_data_loader/data_resampler.py
@@ -9,27 +9,36 @@ from datetime import datetime, timedelta
 import numpy as np
 
 
-def getResampleTimeWindowForPreviousHour(dateTimeObj):
-    """Function that returns a resample time window (1 hour) based on the previous hour
-    
-    Returns 'resampleWindowStart' and 'resampleWindowEnd' as datetime objects
-    """
 
-    # Check if given argument is of type datetime
+def getResampleTimeWindowForPreviousHour(dateTimeObj):
+    
     if isinstance(dateTimeObj, datetime):        
         print("Trigger time was " + dateTimeObj.strftime("%Y-%m-%d, %H:%M:%S"))
     else:
         print("Error: Datatype 'datatime object' expected ")
-        return
+        sys.exit()
     
-    # Substract one hour from the provided time
-    resampleWindow = dateTimeObj - timedelta(hours = 1)
-    # Set 'resample window start' to the previous full hour     
-    resampleWindowStart = datetime(dateTimeObj.year, dateTimeObj.month, dateTimeObj.day, resampleWindow.hour)
-    # Add one hour to obtain the 'resample window end' 
-    resampleWindowEnd = resampleWindowStart + timedelta(hours = 1)
-    # Return start/end of resample window as datatime objects in a tuple
+    # If the trigger was just arround midnight, then we need to handle this individually!
+    if dateTimeObj.hour == 0:
+
+        # Substract one hour from the provided time
+        resampleWindow = dateTimeObj - timedelta(hours = 1)
+        # At midnight we need to consider the day when setting WindowStart
+        # e.g given time: 00:15:00 --> 23:00:00
+        resampleWindowStart = datetime(dateTimeObj.year, dateTimeObj.month, resampleWindow.day, resampleWindow.hour)
+        resampleWindowEnd = resampleWindowStart + timedelta(hours = 1)
+
+    # In any other hour but midnight we can merely subract one hour. That's it!
+    else:
+        # Substract one hour from the provided time
+        resampleWindow = dateTimeObj - timedelta(hours = 1)
+        # Use the given date but only use previous hour instead of entire time
+        # e.g given time: 11:15:00 --> 10:00:00
+        resampleWindowStart = datetime(dateTimeObj.year, dateTimeObj.month, dateTimeObj.day, resampleWindow.hour)
+        resampleWindowEnd = resampleWindowStart + timedelta(hours = 1)
+    
     return [resampleWindowStart, resampleWindowEnd] 
+
 
 
 
